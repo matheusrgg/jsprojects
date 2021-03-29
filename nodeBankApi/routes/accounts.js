ec5af02c4379b96a5ffb6e2a6d880c0679d520b7
matheusrgg <matheusrgg@gmail.com>
2021-03-29 10:48:53 -0300

    defining global variables

--- nodeBankApi/routes/accounts.js
+++ nodeBankApi/routes/accounts.js
@@ -18,17 +18,20 @@ router.post("/", async (req, res) => {
         // data.nextId++
         data.accounts.push(account);
 
-        await writeFile("accoutns.json", JSON.stringify(data));
-
-
+        await writeFile("accounts.json", JSON.stringify(data, null , 2));
         res.send(account);
     } catch (error) {
         res.status(400).send({error: error.message});
     }
-
-
-
 });
+
+router.get("/", async (req, res)=>{
+    try{
+        const data = JSON.parse(await readFile("accounts.json"));
+    } catch(err){
+        res.status(400).send({error: err.message });
+    }
+})
 
 
 
