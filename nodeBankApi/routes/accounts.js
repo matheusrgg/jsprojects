a55a564f62f716cc6d836829883e357de865b9c6
matheusrgg <matheusrgg@gmail.com>
2021-03-29 16:19:27 -0300

    put and patch

--- nodeBankApi/routes/accounts.js
+++ nodeBankApi/routes/accounts.js
@@ -60,7 +60,45 @@ router.delete("/:id", async (req, res)=>{
     } catch(err){
         res.status(400).send({error: err.message });
     }
-})
+});
+
+router.put("/", async (req, res)=>{
+    try{
+
+        const account = req.body;
+        const data = JSON.parse(await readFile(global.fileName));
+
+        const index = data.accounts.findIndex(a => a.id === account.id);
+
+        data.accounts[index] = account;
+
+ 
+        await writeFile(global.fileName, JSON.stringify(data));
+        res.send(account);
+    } catch(err){
+        res.status(400).send({error: err.message });
+    }
+});
+
+
+router.patch("/updateBalance", async (req, res)=>{
+    try {
+
+        const account = req.body;
+        const data = JSON.parse(await readFile(global.fileName));
+
+        const index = data.accounts.findIndex(a => a.id === account.id);
+
+        data.accounts[index].balance = account.balance;
+
+ 
+        await writeFile(global.fileName, JSON.stringify(data));
+        res.send(  data.accounts[index]);
+        
+    } catch (error) {
+        res.status(400).send({error: err.message });
+    }
+});
 
 
 export default router;
