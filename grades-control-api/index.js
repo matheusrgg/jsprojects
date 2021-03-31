44e6a527bc890f92aa36a97f52adde8b42226adb
matheusrgg <matheusrgg@gmail.com>
2021-03-31 11:12:59 -0300

    HTTP method GET working

--- grades-control-api/index.js
+++ grades-control-api/index.js
@@ -1,11 +1,31 @@
 import express from "express";
 import gradesRouter from "./routes/grades.js";
+import { promises as fs } from "fs";
+
+//vamos estar desistruturando
+const { readFile , writeFile } = fs;
+
+global.fileName = "grades.json";
 
 const app = express();
 app.use(express.json());
 
-app.use("/account", gradesRouter);
+app.use("/grade", gradesRouter);
 
-app.listen(3000, ()=>{
-	console.log("Api Started");
+app.listen(3000, async ()=>{
+    try {
+        await readFile(global.fileName);
+        console.log("Api Started !");
+    } catch (error) {
+        
+        const initialJson ={
+            nextId: 1,
+            grades: []
+        }
+        writeFile(global.fileName, JSON.stringify(initialJson)).then(()=>{
+            console.log("Api Started and File Created!");
+        }).catch(error =>{
+            console.log(error);
+        });
+    }
 });
