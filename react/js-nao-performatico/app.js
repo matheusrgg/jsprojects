2342e01accdbcb76216f4e9ea18932e016a9bc5c
matheusrgg <matheusrgg@gmail.com>
2021-04-07 16:27:11 -0300

    aula 4 de react consegui arrumar aqui , embaçado

---
+++ react/js-nao-performatico/app.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', start);
+
+const clickArray = [];
+
+function start() {
+  const button = document.querySelector('#clickButton');
+  button.addEventListener('click', handleButtonClick);
+}
+
+function handleButtonClick() {
+  const now = new Date();
+  clickArray.push(now.toISOString());
+
+  console.log(clickArray);
+  render();
+}
+
+function render() {
+  const ul = document.querySelector('#data');
+  ul.innerHTML = '';
+
+  let lis = '';
+  //item vetor com data e hora
+  clickArray.map((item) => {
+    lis += `<li>${item}</li>`;
+  });
+
+  ul.innerHTML = lis;
+
+
+}
