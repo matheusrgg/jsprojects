663e62d2e823dea2e0a28264b4723c060083f2a3
matheusrgg <matheusrgg@gmail.com>
2021-04-14 20:57:27 -0300

    add codewars

---
+++ codewars/8kyu/basicDataTypeObject/app.js
@@ -0,0 +1,10 @@
+// var animal = {name:"dog",legs:4,color:"white"}
+
+function animal(obj){
+    console.log("This " + obj.color + " " + obj.name + " has " + obj.legs + " legs");
+}
+
+animal( animal = {name:"dog",legs:4,color:"white"});
+
+  
+  
