48cb382d29f3d833f794c9bb13decf234203a403
matheusrgg <matheusrgg@gmail.com>
2021-04-17 22:50:54 -0300

    else if was wrong, now is correct

--- codewars/6kyu/app.js
+++ codewars/6kyu/app.js
@@ -15,11 +15,21 @@ function likes(names) {
   else if (names.length === 3)
     return( `${names[0]}` + ',' + ' ' + `${names[1]}` + ' ' + 'and' + ' ' + `${names[2]}` + ' ' + 'like this')
 
-  else if(names.length === 4)
-    var teste = names.length - 2
-
-  return( `${names[0]}` + ',' + ' ' + `${names[1]}` + ' ' + 'and' + ' ' + teste + ' ' + 'others like this')
+  else 
+    var teste = names.length -2; 
+    return( `${names[0]}` + ',' + ' ' + `${names[1]}` + ' ' + 'and' + ' ' + teste + ' ' + 'others like this')
 }
 
 
-likes(['Max', 'John', 'John', 'John', 'John' , 'John'])
+--------------- resolucao -------
+
+function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
