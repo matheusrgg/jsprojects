4096780135ec60d5e1d466702ac50367d268fcfa
matheusrgg <matheusrgg@gmail.com>
2021-05-03 21:49:38 -0300

    codewars

---
+++ codewars/8kyu/numberObject/app.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/5722fd3ab7162a3a4500031f/train/javascript
+
+function whatNumberIsIt(n) {
+  //coding here
+  switch (n) {
+    case (n === 1 / 0):
+      return "Input number is Number.POSITIVE_INFINITY";
+      break;
+    case (n === 100):
+      return "Input number is 100";
+      break;
+
+
+  }
+
+
+}
