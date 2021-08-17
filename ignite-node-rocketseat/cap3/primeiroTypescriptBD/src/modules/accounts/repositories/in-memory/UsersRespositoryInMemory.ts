2f25c16c70d062d4bbd8b81056eaaf4ce51dea41
matheusrgg <matheusrgg@gmail.com>
2021-08-17 00:16:42 -0300

    comecando quarto capitulo

--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/accounts/repositories/in-memory/UsersRespositoryInMemory.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/accounts/repositories/in-memory/UsersRespositoryInMemory.ts
@@ -1,5 +1,5 @@
-import { User } from "../../infra/typeorm/entities/User";
 
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../IUsersRepository";
 
