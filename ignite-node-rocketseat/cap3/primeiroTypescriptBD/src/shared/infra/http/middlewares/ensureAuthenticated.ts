d23044f65d93e61f703d8657dce1e814b29dc876
matheusrgg <matheusrgg@gmail.com>
2021-08-19 19:51:50 -0300

    erro ao criar carro arrumado

--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -12,26 +12,23 @@ export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction
-) {
+): Promise<void> {
   const authHeader = request.headers.authorization;
+
   if (!authHeader) {
-    throw new AppError("Token missing", 401)
+    throw new AppError("Token missing", 401);
   }
 
-  const [, token] = authHeader.split(" ")
+  const [, token] = authHeader.split(" ");
   try {
     const { sub: user_id } = verify(token, "7697bf58e5a63d82d368d72ccd0bef8f") as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    request.user = {
+      id: user_id,
+    };
 
-    if (!user) {
-      throw new AppError("User does not exists!", 401);
-    }
-
-    next()
+    next();
   } catch (error) {
-    throw new AppError("Invalid Token!", 401);
+    throw new AppError("Invalid token!", 401);
   }
-
 }
