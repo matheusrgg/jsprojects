4677a7e9143cf0436602188e31fe5323550182cf
matheusrgg <matheusrgg@gmail.com>
2021-08-24 13:08:31 -0300

    tudo ok -- seguindo paracontinuaçao da listagem de carros disponiveis

--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/listCars/ListAvailableCarsUseCase.spec.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/listCars/ListAvailableCarsUseCase.spec.ts
@@ -13,8 +13,42 @@ describe("List Cars", () => {
 
   it("should be able to list all available cars", async () => {
 
-    const cars = await listAvailableCarsUseCase.execute();
-    console.log(cars);
-  })
+    const car = await carsRepositoryInMemory.create({
+      name: "Car1",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1234",
+      fine_amount: 40,
+      brand: "Car_brand",
+      category_id: "category_id",
+    });
 
-});
+    const cars = await listAvailableCarsUseCase.execute({
+
+    });
+
+    console.log(cars);
+
+    expect(cars).toEqual([car]);
+
+  });
+
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1235",
+      fine_amount: 40,
+      brand: "Car_brand_test",
+      category_id: "category_id",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({
+      name: "Car3",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+})
--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/listCars/ListAvailableCarsUseCase.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/listCars/ListAvailableCarsUseCase.ts
@@ -2,9 +2,16 @@
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
+
+interface IRequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
+
 class ListAvailableCarsUseCase {
   constructor(private carsRepository: ICarsRepository) { }
-  async execute(): Promise<Car[]> {
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable();
     return cars;
   }
