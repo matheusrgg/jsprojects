c9808648c83dce6a58f0b9b5a178a37731f73ce1
matheusrgg <matheusrgg@gmail.com>
2021-08-29 21:54:53 -0300

    error invalid return time createRental

--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/rentals/useCases/CreateRentallUseCase.spec.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/rentals/useCases/CreateRentallUseCase.spec.ts
@@ -1,27 +1,107 @@
+import dayjs from "dayjs";
 
+import {DayjsDateProvider} from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { CreateRentalUseCase } from "./CreateRentallUseCase";
 import { RentalsRepositoryInMemory } from "../infra/typeorm/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
-
+let dayjsDateProvider: DayjsDateProvider;
 
 
 
 
 describe("Create Rental", () => {
+
+  const dayAdd24Hours = dayjs().add(1, "day").toDate();
+
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
-    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+    dayjsDateProvider = new DayjsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepositoryInMemory,
+      dayjsDateProvider,
+      );
   })
 
   it("should be able to create a new rental", async () => {
+
+    // const car = await carsRepositoryInMemory.create({
+    //   name: "Test",
+    //   description: "Car Test",
+    //   daily_rate: 100,
+    //   license_plate: "test",
+    //   fine_amount: 40,
+    //   category_id: "1234",
+    //   brand: "brand",
+    // });
+
+    const rental = await createRentalUseCase.execute({
+       user_id: "12345",
+       car_id: "121212",
+       expected_return_date: dayAdd24Hours,
+     })
+ 
+ 
+     console.log(rental);
+ 
+     expect(rental).toHaveProperty("id");
+     expect(rental).toHaveProperty("start_date")
+   })
+
+
+   
+   it(" should not be able to create a new rental if there is another open to the same user ", async () => {
+
+    expect( async () =>{
+
+      await createRentalUseCase.execute({
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+
     await createRentalUseCase.execute({
-      user_id: "12345",
-      car_id: "121212",
-      expected_return_date: new Date(),
-    })
-  })
-})
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+    
+  });
+
+
+
+  it(" should not be able to create a new rental if there is another open to the same car ", async () => {
+
+    expect( async () =>{
+
+      await createRentalUseCase.execute({
+        user_id: "123",
+        car_id: "teste",
+        expected_return_date: dayAdd24Hours,
+      });
+
+    await createRentalUseCase.execute({
+        user_id: "321",
+        car_id: "test",
+        expected_return_date: dayAdd24Hours,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+    
+  });
+
+  it(" should not be able to create a new rental with invalid return time ", async () => {
+ 
+      await createRentalUseCase.execute({
+        user_id: "123",
+        car_id: "test",
+        expected_return_date: dayjs().toDate(),
+      })
+
+  });
+
+});
--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/rentals/useCases/CreateRentallUseCase.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/rentals/useCases/CreateRentallUseCase.ts
@@ -1,7 +1,7 @@
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
+import { Rental } from "../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../infra/typeorm/repositories/IRentalsRepository";
-
-
 
 interface IRequest {
   user_id: string;
@@ -9,21 +9,20 @@ interface IRequest {
   expected_return_date: Date;
 }
 
-
-
-
-
-
 class CreateRentalUseCase {
 
-  constructor(private rentalsRepository: IRentalsRepository) { }
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider,
+    
+    ) { }
 
   async execute({
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
-
+  }: IRequest): Promise<Rental> {
+    const minimumHour = 24;
     // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro
 
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
@@ -46,9 +45,24 @@ class CreateRentalUseCase {
 
     //O aluguel deve ter duração mpinima de 24 horas
 
+    const dateNow = this.dateProvider.dateNow();
 
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
 
+    
+    if(compare< minimumHour){
+      throw new AppError("Invalid return time")
+    }
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date
+    })
 
+    return rental;
 
   }
 }
