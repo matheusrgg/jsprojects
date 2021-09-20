5d169b9d57f7462eace68d865e401b2d9e522bad
matheusrgg <matheusrgg@gmail.com>
2021-09-20 14:06:56 -0300

    fim cap 4

--- ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ ignite-node-rocketseat/cap3/primeiroTypescriptBD/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -29,7 +29,7 @@ class CreateCategoryUseCase {
       throw new AppError("Category Already exists !")
     }
 
-    this.categoriesRepository.create({ name, description });
+    await this.categoriesRepository.create({ name, description });
 
   }
 }
